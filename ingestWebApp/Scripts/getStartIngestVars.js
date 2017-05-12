8bb58103c05b542911634ddf0b1bae74ac91e70c
Alex Banakas <abanakas@gmail.com>
2017-05-12 08:59:00 -0400

    Made changes to packages and got localStorage working

---
+++ ingestWebApp/Scripts/getStartIngestVars.js
@@ -0,0 +1,6 @@
+﻿window.onload = function () {
+    var sourceName = localStorage.getItem("sName");
+    var sourcePath = localStorage.getItem("sPath");
+    document.getElementById("sName").innerHTML = sourceName;
+    document.getElementById("sPath").innerHTML = sourcePath;
+}
---
+++ ingestWebApp/Scripts/setStartIngestVars.js
@@ -0,0 +1,8 @@
+﻿function getIngestData() {
+    var sourceName = document.getElementById("sName").value;
+    var sourcePath = document.getElementById("sPath").value;
+    localStorage.setItem("sName", sourceName);
+    localStorage.setItem("sPath", sourcePath);
+    //alert(sName);
+    //alert(sPath);
+}
